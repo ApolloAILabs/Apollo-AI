5d7bcc2b90d3290f2335ee47f877c3fe6b9f756e
ApolloAILabs <luckywayne88@icloud.com>
2024-05-27 20:11:43 -0400

    apollo ai beta 1.0
    added simple linear regression algorithm for algebraic equations

--- code/main.py
+++ code/main.py
@@ -1,6 +1,26 @@
-print("Welcome to Apollo Ai")
-user_selection = input("type 1 to use the number recognition neural network")
-if(user_selection == "1"):
-    print("number recognition is a work in progress :)") 
-else:
-    print("thats either not one of our neural nets or youre typing giberish :D")
+# this comment was made to edit the file in order to fix github pushing issues
+
+from random import randint
+TRAIN_SET_LIMIT = 1000
+TRAIN_SET_COUNT = 100
+
+TRAIN_INPUT = list()
+TRAIN_OUTPUT = list()
+for i in range(TRAIN_SET_COUNT):
+    a = randint(0, TRAIN_SET_LIMIT)
+    b = randint(0, TRAIN_SET_LIMIT)
+    c = randint(0, TRAIN_SET_LIMIT)
+    op = a + (2*b) + (3*c)
+    TRAIN_INPUT.append([a, b, c])
+    TRAIN_OUTPUT.append(op)
+
+from sklearn.linear_model import LinearRegression
+
+predictor = LinearRegression(n_jobs=-1)
+predictor.fit(X=TRAIN_INPUT, y=TRAIN_OUTPUT)
+
+X_TEST = [[3, 6, 9]]
+outcome = predictor.predict(X=X_TEST)
+coefficients = predictor.coef_
+
+print('Outcome : {}\nCoefficients : {}'.format(outcome, coefficients))
